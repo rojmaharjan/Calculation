a744ab25d2ddd07324f3da909d1c012374503094
Roj Maharjan <84655184+rojmaharjan@users.noreply.github.com>
2021-08-01 16:06:22 +0545

    Add files via upload

---
+++ 3maths.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+//maths calculations
+	
+//Declaring variables
+	int firstnumber = 50 ;
+	int secondnumber = 25 ;
+	
+//add, subtract, division, multiply
+	int add, sub, div, mul ;
+
+//calculating
+	add = firstnumber + secondnumber ;
+	sub = firstnumber - secondnumber ;
+	div = firstnumber / secondnumber ;
+	mul = firstnumber * secondnumber ;
+	
+//Printing
+	printf("First Number = 50 \nSecond Number = 25 \n");
+	printf("The sum of 1st and 2nd is %d\n",add);
+	printf("The subration of 1st and 2nd is %d\n",sub);
+	printf("The division of 1st and 2nd is %d\n",div);
+	printf("The multiplication of 1st and 2nd is %d\n",mul);
+	
+	return 0;
+}
